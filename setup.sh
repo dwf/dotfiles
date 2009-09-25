e71fcf7c41b2e078b1f66be5b15a0d00bb96e6a5
David Warde-Farley <dwf@rodimus.(none)>
2009-09-25 02:44:46 -0400

    Better/more complete install script.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-function install_script()
+function install_dotfile()
 {
-    if [ -r ~/"$1" ] ; then
-        mv -v "$HOME/$1" "$HOME/$1.backup.`date |tr \" :\" \"__\"`"
+    # If there's a regular file there (not symlink), make a backup 
+    if [ -r "$HOME/$1" ] ; then
+        mv "$HOME/$1" "$HOME/$1.backup"
     fi
-    if [ \! -e "`dirname $1`" ] ; then
-        mkdir -p "`dirname $1`"
+    if [ \! -e "$HOME/`dirname $1`" ] ; then
+        mkdir -v -p "$HOME/`dirname $1`"
     fi
-    cp -v "./$1" "$HOME/$1"
+    # If there is no symlink yet, then link it already.
+    if [ \! -h "$HOME/$1" ] ; then
+        echo -n 'Symlinking: '
+        ln -v -s "./$1" "$HOME/$1"
+    fi
 }
 
-install_script .bash_profile
-install_script .bashrc
-install_script .vimrc
-install_script .prompt.sh
-install_script .ssh/config
+# Bash configuration
+install_dotfile .bash_profile
+install_dotfile .bashrc
+
+# Git-related
+install_dotfile .git-bash-completion.sh
+install_dotfile .gitconfig
+
+# Prompt colouring
+install_dotfile .prompt.sh
+
+# Settings for GNU screen
+install_dotfile .screenrc
+
+# ssh configuration
+install_dotfile .ssh/config
+
+# Plugins bindings for vim
+install_dotfile .vim/ftplugin/python.vim
+install_dotfile .vim/indent/python.vim
+
+# Global settings for vim
+install_dotfile .gvimrc
+install_dotfile .vimrc
