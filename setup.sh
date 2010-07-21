462b1217297d27b0363e04db262b6245658c85b9
David Warde-Farley <dwf@cs.toronto.edu>
2010-07-20 21:10:02 -0400

    Add the new syntax/python.vim to setup.sh.

--- setup.sh
+++ setup.sh
@@ -42,6 +42,7 @@ install_dotfile .vim/ftplugin/opencl.vim
 install_dotfile .vim/ftdetect/opencl.vim
 install_dotfile .vim/indent/opencl.vim
 install_dotfile .vim/syntax/opencl.vim
+install_dotfile .vim/syntax/python.vim
 install_dotfile .vim/syntax/pyrex.vim
 install_dotfile .vim/ftdetect/f2py.vim
 install_dotfile .vim/ftplugin/python/pyflakes.vim
