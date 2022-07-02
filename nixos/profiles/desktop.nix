a137a93c6ee53f7a219d74c4af90139c234bfbac
David Warde-Farley <dwf@google.com>
2022-07-02 01:17:57 +0100

    desktop.nix: flatpak (for Steam)

--- nixos/profiles/desktop.nix
+++ nixos/profiles/desktop.nix
@@ -1,17 +1,26 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    displayManager = {
-      lightdm = {
-        enable = true;
-        greeters.gtk.enable = true;
-      };
+  services = {
+    xserver = {
+      enable = true;
+      layout = "us";
+      displayManager = {
+        lightdm = {
+          enable = true;
+          greeters.gtk.enable = true;
+        };
 
-      # Defined in nixos/modules/user-xsession.nix
-      defaultSession = lib.mkDefault "user-xsession";
+        # Defined in nixos/modules/user-xsession.nix
+        defaultSession = lib.mkDefault "user-xsession";
+      };
     };
+    flatpak.enable = true;
+    accounts-daemon.enable = true;  # Required for flatpak+xdg
+  };
+
+  xdg.portal = {
+    enable = true;  # xdg portal is used for tunneling permissions to flatpak
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
   # SSD with full disk encryption (except for boot EFI), including swap.
@@ -23,5 +32,8 @@
 
   services.printing.enable = true;
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware = {
+    pulseaudio.enable = true;
+    opengl.driSupport32Bit = true;
+  };
 }
