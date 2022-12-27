00f75febbd3240c4a2467b40739f598b789fe994
David Warde-Farley <dwf@google.com>
2022-12-27 18:53:25 +0000

    shockwave: Switch to argononed.

--- nixos/hosts/shockwave/default.nix
+++ nixos/hosts/shockwave/default.nix
@@ -1,17 +1,4 @@
 { pkgs, ...}:
-let
-  argonone-utils = pkgs.buildGoModule rec {
-    name = "argonone-utils";
-    version = "0.1.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "mgdm";
-      repo = "argonone-utils";
-      rev = "v${version}";
-      sha256 = "040319yrdsbkjwqvgq2pgsq0hxm42ps9nbv8s0vcwxc7v7l9la6d";
-    };
-    vendorSha256 = "18qwmg249lr7xb9f7lrrflhsr7drx750ndqd8hirq5hgj4c4f66k";
-  };
-in
 {
   imports =
     [
@@ -49,15 +36,7 @@ in
   hardware.i2c.enable = true;
   hardware.raspberry-pi."4".i2c1.enable = true;
 
-  systemd.services.argonone-fancontrold = {
-    enable = true;
-    wantedBy = [ "default.target" ];
-    serviceConfig = {
-      DynamicUser = true;
-      Group = "i2c";
-      ExecStart = "${argonone-utils}/bin/argonone-fancontrold";
-    };
-  };
+  services.hardware.argonone.enable = true;
 
   services.printing = {
     enable = true;
