387005880bb572378f165cf111bf320ea38c5c2e
David Warde-Farley <dwf@google.com>
2023-02-08 15:21:47 +0000

    neovim: another deprecation

--- home-manager/modules/nvim-lsp.nix
+++ home-manager/modules/nvim-lsp.nix
@@ -128,7 +128,7 @@ let
       # TODO(dwf): Make more configurable.
       "  vim.api.nvim_command(\"augroup lsp_highlighting\")"
       "  vim.api.nvim_command(\"autocmd!\")"
-      "  if client.resolved_capabilities.document_highlight then"
+      "  if client.server_capabilities.document_highlight then"
       "    vim.api.nvim_command(\"autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()\")"
       "    vim.api.nvim_command(\"autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()\")"
       "    vim.api.nvim_command(\"autocmd CursorMoved <buffer> lua vim.lsp.util.buf_clear_references()\")"
