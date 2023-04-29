19db27328bc987afc5d09db3a7abe374f9b1e004
David Warde-Farley <dwf@google.com>
2023-04-29 11:17:46 +0100

    bumblebee: remove novnc overlay

--- nixos/hosts/bumblebee/default.nix
+++ nixos/hosts/bumblebee/default.nix
@@ -4,25 +4,6 @@ let
   tailscaleDomain = config.services.tailscaleHttpsReverseProxy.tailscaleDomain;
 in
 {
-  # TODO(dwf): Remove this overlay when the patch hits stable.
-  nixpkgs.overlays = [
-    (self: super: let
-      websockifyPatch = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/13cc17cc53eab4a59fe5530a53faef1eabaafc0f/pkgs/applications/networking/novnc/websockify.patch";
-        sha512 = "327032c1f8e1a5b89f1ce355fdc3d534afd3c9740d4e7963781857409299e2a4f9687fd14be8a07e5f81b5a6a1d97dfae8d9ff62cdc665b8324926c65f41eab8";
-      };
-    in {
-      novnc = super.novnc.overrideAttrs (old: {
-        patches = with pkgs.python3.pkgs; [
-          (pkgs.substituteAll {
-            src = websockifyPatch;
-            inherit websockify;
-          })
-        ];
-      });
-    })
-  ];
-
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
