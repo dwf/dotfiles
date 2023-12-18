78e073ffe77999bb200fddf23e63e59dc1c1cf59
David Warde-Farley <dwf@google.com>
2023-12-18 23:16:25 +0000

    hm/superion: keychain support

--- home-manager/hosts/superion/default.nix
+++ home-manager/hosts/superion/default.nix
@@ -6,5 +6,13 @@
     ../../profiles/x11/laptop.nix
   ];
 
+  programs.keychain = {
+    enable = true;
+    enableBashIntegration = true;
+    enableXsessionIntegration = true;
+    agents = [ "ssh" ];
+    keys = [ "id_ed25519" ];
+  };
+
   services.picom.vSync = true;
 }
