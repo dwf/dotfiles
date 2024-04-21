ed0237b5452978fc523179fb914347d03fa9fec0
David Warde-Farley <dwf@google.com>
2024-04-21 17:48:59 +0100

    steam.nix: declaratively permission /mnt/steam

--- nixos/profiles/desktop/steam.nix
+++ nixos/profiles/desktop/steam.nix
@@ -5,4 +5,8 @@
     inherit (config.fileSystems."/") device fsType;
     options = [ "defaults" "noatime" "compress=zstd" "autodefrag" "subvol=@steam" "user" "exec" ];
   };
+
+  systemd.tmpfiles.rules = [
+    "d /mnt/steam 0755 dwf users"
+  ];
 }
