f68a4ee47f2067fabfadbdceeb9cd2dfa7daa2cf
David Warde-Farley <dwf@google.com>
2024-05-06 01:54:07 +0100

    hm/superion: enable redshift

--- home-manager/hosts/superion/default.nix
+++ home-manager/hosts/superion/default.nix
@@ -11,5 +11,13 @@
     extraPackages = tpkgs: { inherit (tpkgs) scheme-small; };
   };
 
+  services.redshift = {
+    enable = true;
+    provider = "manual";
+    latitude = "51.5007";
+    longitude = "0.1246";
+    tray = true;
+  };
+
   services.picom.vSync = true;
 }
