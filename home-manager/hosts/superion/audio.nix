7d319edfb1fd593675919dbdc18d3266fb3a5105
David Warde-Farley <dwf@google.com>
2024-05-06 16:30:47 +0100

    hm/superion: easyeffects service definition

--- home-manager/hosts/superion/audio.nix
+++ home-manager/hosts/superion/audio.nix
@@ -1,4 +1,9 @@
 { inputs, ... }:
 {
   xdg.configFile."easyeffects/output".source = inputs.framework-audio-presets.outPath;
+
+  services.easyeffects = {
+    enable = true;
+    preset = "kieran_levin";
+  };
 }
