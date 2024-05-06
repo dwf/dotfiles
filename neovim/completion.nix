a29c6854cec91ff20055f6ad10c8fb5a31f58e9c
David Warde-Farley <dwf@google.com>
2024-05-06 21:58:20 +0100

    neovim/completion.nix: key mappings

--- neovim/completion.nix
+++ neovim/completion.nix
@@ -14,6 +14,11 @@
     ];
     mapping = {
       "<CR>" = "cmp.mapping.confirm({ select = false })";
+      "<C-Space>" = "cmp.mapping.confirm({ select = true })";
+      "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+      "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      "<S-Tab>" = "cmp.mapping.complete()";
+      "<Esc>" = "cmp.mapping.close()";
     };
     preselect = "None";
     experimental = {
