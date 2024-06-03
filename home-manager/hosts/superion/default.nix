acaa6eadf9bc765113059339b0ee8f7d629e28be
David Warde-Farley <dwf@google.com>
2024-06-03 22:29:59 +0100

    hm: support redshift/gammastep

--- home-manager/hosts/superion/default.nix
+++ home-manager/hosts/superion/default.nix
@@ -11,13 +11,5 @@
     extraPackages = tpkgs: { inherit (tpkgs) scheme-small; };
   };
 
-  services.redshift = {
-    enable = true;
-    provider = "manual";
-    latitude = "51.5007";
-    longitude = "0.1246";
-    tray = true;
-  };
-
   services.picom.vSync = true;
 }
---
+++ home-manager/profiles/desktop/color-temperature.nix
@@ -0,0 +1,13 @@
+{ config, lib, ... }:
+let
+  cfg = {
+    enable = true;
+    provider = "manual";
+    latitude = "51.5007";
+    longitude = "0.1246";
+    tray = config.xsession.enable;  # gammastep just gives me a red frowny face
+  };
+in {
+  services.redshift = lib.mkIf config.xsession.enable cfg;
+  services.gammastep = lib.mkIf (! config.xsession.enable) cfg;
+}
