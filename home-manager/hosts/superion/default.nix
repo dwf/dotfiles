87167836762c0b9e6069d03e5034dd7988086d66
David Warde-Farley <dwf@google.com>
2024-06-09 23:41:35 +0100

    hm/superion: dirty swayosd/easyeffects hack

--- home-manager/hosts/superion/default.nix
+++ home-manager/hosts/superion/default.nix
@@ -7,6 +7,17 @@
     ./audio.nix
   ];
 
+  # TODO(dwf): Remove this when the bug is fixed upstream
+  nixpkgs.overlays = [
+    (self: super: {
+      swayosd = super.swayosd.overrideAttrs (old: {
+        # Hardcode the sink name I need as the command-line argument isn't 
+        # actually fed through.
+        patches = old.patches ++ [ ./swayosd-hardcode-easyeffects.patch ];
+      });
+    })
+  ];
+
   programs.texlive = {
     enable = true;
     extraPackages = tpkgs: { inherit (tpkgs) scheme-small; };
