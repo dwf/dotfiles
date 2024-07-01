177dc0838b906b08b431df6441270128ec7aee33
David Warde-Farley <dwf@google.com>
2024-07-01 21:58:11 +0100

    neovim: lsp keymaps

--- neovim/lsp.nix
+++ neovim/lsp.nix
@@ -1,9 +1,28 @@
 {
   config.plugins.lsp = {
     enable = true;
-    keymaps.diagnostic = {
-      "]d" = "goto_next";
-      "[d" = "goto_prev";
+    keymaps = {
+      diagnostic = {
+        "]d" = "goto_next";
+        "[d" = "goto_prev";
+      };
+      lspBuf = {
+        "<Leader>rn" = "rename";
+        "<Leader>ca" = "code_action";
+        "<C-k>" = "signature_help";
+        K = "hover";
+        g0 = "document_symbol";
+        gW = "workspace_symbol";
+        gd = "definition";
+        gD = "declaration";
+        gi = "implementation";
+        gr = "references";
+        gt = "type_definition";
+      };
+      diagnostic = {
+        "]d" = "goto_next";
+        "[d" = "goto_prev";
+      };
     };
     servers = {
       bashls.enable = true;
