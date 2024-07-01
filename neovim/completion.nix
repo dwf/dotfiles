105bc79c6f97d172908016b582eceded6744b344
David Warde-Farley <dwf@google.com>
2024-07-01 23:20:35 +0100

    neovim: nvim-cmp key bindings

--- neovim/completion.nix
+++ neovim/completion.nix
@@ -16,10 +16,13 @@
       ];
       mapping = {
         "<CR>" = "cmp.mapping.confirm({ select = false })";
-        "<C-Space>" = "cmp.mapping.confirm({ select = true })";
+        "<C-Space>" = "cmp.mapping(cmp.mapping.complete(), {'i', 'c'})";
         "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         "<S-Tab>" = "cmp.mapping.complete()";
+        "<C-u>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-d>" = "cmp.mapping.scroll_docs(4)";
+        "<C-e>" = "cmp.mapping.close()";
       };
       preselect = "None";
       experimental = {
