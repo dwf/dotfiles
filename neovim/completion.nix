0e6cd3ea06c49e87b9180d3ec71376a378cde1cf
David Warde-Farley <dwf@google.com>
2024-08-24 22:40:04 +0100

    neovim: remove <S-Tab> cmp mapping

--- neovim/completion.nix
+++ neovim/completion.nix
@@ -19,7 +19,6 @@
         "<C-Space>" = "cmp.mapping(cmp.mapping.complete(), {'i', 'c'})";
         "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-        "<S-Tab>" = "cmp.mapping.complete()";
         "<C-u>" = "cmp.mapping.scroll_docs(-4)";
         "<C-d>" = "cmp.mapping.scroll_docs(4)";
         "<C-e>" = "cmp.mapping.close()";
