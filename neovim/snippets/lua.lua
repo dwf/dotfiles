77cdbbe525e300507fbf2b6c0ad5d43a45945c29
David Warde-Farley <dwf@google.com>
2024-08-26 18:07:16 +0100

    neovim: snippets for authoring snippets

--- neovim/snippets/lua.lua
+++ neovim/snippets/lua.lua
@@ -2,6 +2,7 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 return {
   s({ trig = "req", desc = "require(...)" }, {
@@ -18,4 +19,45 @@ return {
     t('")'),
     i(0),
   }),
+  s(
+    { trig = "snip", desc = "Basic luasnip snippet" },
+    fmt(
+      [[
+      s({{ trig = "{}", desc = "{}" }}, {{
+        {}
+      }}){}
+      ]],
+      {
+        i(1, "trigger"),
+        i(2, "description"),
+        i(3, "-- nodes"),
+        i(0),
+      }
+    )
+  ),
+  s(
+    { trig = "fsnip", desc = "Formatted luasnip snippet" },
+    fmt(
+      [=[
+        s(
+          {{ trig = "{}", desc = "{}" }},
+          fmt(
+            [[
+            {}
+            ]],
+            {{
+              {}
+            }}
+          )
+        ){}
+      ]=],
+      {
+        i(1, "trigger"),
+        i(2, "description"),
+        i(3, "-- text"),
+        i(4, "-- nodes"),
+        i(0),
+      }
+    )
+  ),
 }, {}
