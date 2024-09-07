ccb7bc1d4d3f26a6470964086370bf352411c188
David Warde-Farley <dwf@google.com>
2024-09-08 00:45:32 +0100

    neovim: correctly add nvim_lua source

--- neovim/completion.nix
+++ neovim/completion.nix
@@ -4,10 +4,10 @@
     autoEnableSources = true;
     settings = {
       sources = [
+        { name = "nvim_lua"; }
         { name = "nvim_lsp"; }
         { name = "nvim_lsp_signature_help"; }
         { name = "path"; }
-        { name = "lua"; }
         {
           name = "buffer";
           option.keyword_length = 5;
