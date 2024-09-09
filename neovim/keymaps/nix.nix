8c354723fdd6a78c76430125f0cd9baa5cdd920f
David Warde-Farley <dwf@google.com>
2024-09-09 11:04:03 +0100

    neovim: simplify nix keymaps

--- neovim/keymaps/nix.nix
+++ neovim/keymaps/nix.nix
@@ -1,67 +1,41 @@
-{ helpers, lib, ... }:
+{ helpers, ... }:
 {
-  config.autoCmd =
-    let
-      nestKeymapsCallbackFn =
-        mappings:
-        let
-          keymapCmd =
-            {
-              key,
-              desc,
-              cmd,
-            }:
-            # lua
-            ''
-              vim.keymap.set( "n", "${key}", "<cmd>${cmd}<cr><cmd>nohl<cr>", { buffer = true, desc = "${desc}"})
-            '';
-        in
+  files."ftplugin/nix.lua".keymaps = [
+    # There is surely a more robust way to do this with treesitter.
+    # N.B. ''double single-quote strings'' with no trailing \n, to
+    # avoid quadruple backslash (one layer for nix, one for lua)
+    {
+      key = "<leader>nnt";
+      action = ''s/^\\([^\\.]\\+\\)\\.\\([^ =]\\+\\)\\ \\?=\\ \\?\\([^;]\\+\\);$/\\1 = { \\2 = \\3; };/e'';
+      mode = [ "n" ];
+      options = {
+        buffer = true;
+        desc = "Nest top level of a one-line dotted attrset assign";
+      };
+    }
+    {
+      key = "<leader>nnb";
+      action = ''s/\\(.\\+\\)\\.\\([^ =]\\+\\)\\ \\?=\\ \\?\\([^;]\\+\\);$/\\1 = { \\2 = \\3; };/e'';
+      mode = [ "n" ];
+      options = {
+        buffer = true;
+        desc = "Nest bottom level of a one-line dotted attrset assign";
+      };
+    }
+    {
+      key = "<leader>ni";
+      action =
         helpers.mkRaw # lua
           ''
             function()
-              vim.schedule(function()
-                ${lib.concatStringsSep "\n" (map keymapCmd mappings)}
-              end)
+              require('treesitter-helpers.nix').maybe_add_module_import(0, vim.fn.expand('<cword>'))
             end
           '';
-    in
-    [
-      {
-        event = [ "FileType" ];
-        pattern = [ "nix" ];
-        callback = nestKeymapsCallbackFn [
-          # There is surely a more robust way to do this with treesitter.
-          # N.B. ''double single-quote strings'' with no trailing \n, to
-          # avoid quadruple backslash (one layer for nix, one for lua)
-          {
-            key = "<leader>nnt";
-            cmd = ''s/^\\([^\\.]\\+\\)\\.\\([^ =]\\+\\)\\ \\?=\\ \\?\\([^;]\\+\\);$/\\1 = { \\2 = \\3; };/e'';
-            desc = "Nest top level of a one-line dotted attrset assign";
-          }
-          {
-            key = "<leader>nnb";
-            cmd = ''s/\\(.\\+\\)\\.\\([^ =]\\+\\)\\ \\?=\\ \\?\\([^;]\\+\\);$/\\1 = { \\2 = \\3; };/e'';
-            desc = "Nest bottom level of a one-line dotted attrset assign";
-          }
-        ];
-      }
-      {
-        event = [ "FileType" ];
-        pattern = [ "nix" ];
-        callback =
-          helpers.mkRaw # lua
-            ''
-              function()
-                vim.schedule(function()
-                vim.keymap.set(
-                  "n",
-                  "<leader>ni",
-                  "<cmd>lua require('treesitter-helpers.nix').maybe_add_module_import(0, vim.fn.expand('<cword>'))<CR>",
-                  { buffer = true, desc = "Add module import for word under cursor (if not already imported)" }
-                )
-                end)
-              end
-            '';
-      }
-    ];
+      mode = [ "n" ];
+      options = {
+        buffer = true;
+        desc = "Add module import for word under cursor (if not already imported)";
+      };
+    }
+  ];
 }
