9e2aa0a26c746268bf4bc21051535ca4ec95c212
David Warde-Farley <dwf@google.com>
2024-09-18 21:50:04 +0100

    overlays/pianoteq.nix: format

--- overlays/pianoteq.nix
+++ overlays/pianoteq.nix
@@ -7,34 +7,46 @@ let
 in
 {
   nixpkgs.overlays = [
-    (self: super: let
-      version = "8.0.8";
-      overriddenPianoteq = name: { filename, sha256, base ? null }: let
-        basePkg = if (isNull base) then name else base;
-      in super.pianoteq."${basePkg}".overrideAttrs (_: {
-        inherit version;
-        pname = "pianoteq-${name}";
-        src = pkgs.fetchurl {
-          inherit sha256;
-          url = "${distRoot}/${filename}";
+    (
+      self: super:
+      let
+        version = "8.0.8";
+        overriddenPianoteq =
+          name:
+          {
+            filename,
+            sha256,
+            base ? null,
+          }:
+          let
+            basePkg = if (isNull base) then name else base;
+          in
+          super.pianoteq."${basePkg}".overrideAttrs (_: {
+            inherit version;
+            pname = "pianoteq-${name}";
+            src = pkgs.fetchurl {
+              inherit sha256;
+              url = "${distRoot}/${filename}";
+            };
+          });
+      in
+      {
+        pianoteq = lib.mapAttrs overriddenPianoteq {
+          standard-trial = {
+            filename = "pianoteq_linux_trial_v808.7z";
+            sha256 = "sha256-LSrnrjkEhsX9TirUUFs9tNqH2A3cTt3I7YTfcTT6EP8=";
+          };
+          stage-trial = {
+            filename = "pianoteq_stage_linux_trial_v808.7z";
+            sha256 = "sha256-dp0bTzzh4aQ2KQ3z9zk+3meKQY4YRYQ86rccHd3+hAQ=";
+          };
+          stage-8 = {
+            filename = "pianoteq_stage_linux_v808.7z";
+            sha256 = "sha256-9TEIKyzKu6fsLGLt+LOHiRHqnwZS0G+QoKw2KUzjwQM=";
+            base = "stage-7";
+          };
         };
-      });
-    in {
-      pianoteq = lib.mapAttrs overriddenPianoteq {
-        standard-trial = {
-          filename = "pianoteq_linux_trial_v808.7z";
-          sha256 = "sha256-LSrnrjkEhsX9TirUUFs9tNqH2A3cTt3I7YTfcTT6EP8=";
-        };
-        stage-trial = {
-          filename = "pianoteq_stage_linux_trial_v808.7z";
-          sha256 = "sha256-dp0bTzzh4aQ2KQ3z9zk+3meKQY4YRYQ86rccHd3+hAQ=";
-        };
-        stage-8 = {
-          filename = "pianoteq_stage_linux_v808.7z";
-          sha256 = "sha256-9TEIKyzKu6fsLGLt+LOHiRHqnwZS0G+QoKw2KUzjwQM=";
-          base = "stage-7";
-        };
-      };
-    })
+      }
+    )
   ];
 }
