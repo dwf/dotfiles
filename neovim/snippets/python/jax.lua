74383eb6aea5a55ba7774a0c38bd5403bcd05b34
David Warde-Farley <dwf@google.com>
2024-10-07 04:29:58 +0100

    neovim: jnp autoimport snippet (treesitter)

--- neovim/snippets/python/jax.lua
+++ neovim/snippets/python/jax.lua
@@ -2,40 +2,25 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local events = require("luasnip.util.events")
+local helpers = require("treesitter-helpers.python")
 
-local function py_manual_autoimport_snippet(import_statement, trigger, line_number_callback, insert_text)
-  -- Manually add an import in some heuristically chosen place on first use of a trigger.
-  if insert_text == nil then
-    insert_text = trigger
+local function autoimport_snippet(import_spec, opts)
+  if opts == nil then
+    opts = {}
   end
-  return s(trigger, { t(insert_text) }, {
+  local imported = import_spec.name
+  if import_spec.alias ~= nil then
+    imported = import_spec.alias
+  end
+  local insert_text = imported .. "."
+  if opts.trigger == nil then
+    opts.trigger = insert_text
+  end
+  return s(opts.trigger, { t(insert_text) }, {
     callbacks = {
       [-1] = {
-
         [events.pre_expand] = function()
-          -- Exactly match the import statement. There could be comments after it or something,
-          -- but that's unlikely for my use cases.
-          local import_match_pattern = "^" .. import_statement .. "$"
-          if vim.b.py_manual_autoimport_cache == nil then
-            vim.b.py_manual_autoimport_cache = {}
-          end
-          local cached_line_num = vim.b.py_manual_autoimport_cache[trigger]
-          if cached_line_num ~= nil then
-            local line = vim.api.nvim_buf_get_lines(0, cached_line_num, cached_line_num, false)[1]
-            if line:match(import_match_pattern) then
-              return
-            end
-          end
-          local matches = vim.fn.matchbufline(vim.fn.bufname(0), import_match_pattern, 1, "$")
-          if #matches > 0 then
-            vim.b.py.manual_autoimport_cache[trigger] = matches[1].lnum
-          else
-            local insert_lnum = line_number_callback()
-            if insert_lnum ~= nil then
-              vim.api.nvim_buf_set_lines(0, insert_lnum, insert_lnum, false, { import_statement })
-              vim.b.py_manual_autoimport_cache[trigger] = insert_lnum
-            end
-          end
+          helpers.maybe_add_import(0, import_spec, opts)
         end,
       },
     },
@@ -43,14 +28,10 @@ local function py_manual_autoimport_snippet(import_statement, trigger, line_numb
 end
 
 return {}, {
-  -- py_manual_autoimport_snippet("import jax.numpy as jnp", "jnp.", function()
-  --   local matches = vim.fn.matchbufline(vim.fn.bufname(0), "^import \\(jax\\|numpy as np\\)$", 1, "$")
-  --   if matches[1] ~= nil and matches[1].lnum ~= nil then
-  --     local lnum = matches[1].lnum
-  --     if matches[1].text:match(".*numpy") then
-  --       lnum = lnum - 1
-  --     end
-  --     return lnum
-  --   end
-  -- end),
+  autoimport_snippet({ name = "jax.numpy", alias = "jnp" }, {
+    placement = {
+      { "after", { name = "jax" } },
+      { "before", { name = "numpy", alias = "np" } },
+    },
+  }),
 }
---
+++ neovim/treesitter/lua/treesitter-helpers/python.lua
@@ -0,0 +1,144 @@
+local M = {}
+
+local function first_import_placement(bufnr, tree)
+  local query = vim.treesitter.query.parse(
+    "python",
+    [[
+      (module
+        (comment)*
+        . (comment)? @last-comment
+        .
+        (expression_statement
+          (string
+            (string_start)
+            (string_content)
+            (string_end)) @module-docstring)?)
+    ]]
+  )
+  local line = 0
+  for _, node, _, _ in query:iter_captures(tree:root(), bufnr) do
+    local _, _, end_line, _ = node:range()
+    line = end_line + 1
+  end
+  return line
+end
+
+local function parse_top_level_imports(bufnr, tree)
+  local query = vim.treesitter.query.parse(
+    "python",
+    [[
+      (module
+      [
+       (import_statement
+         name: (dotted_name) @name @imported)
+       (import_statement
+         name: (aliased_import
+                 name: (dotted_name) @name
+                 alias: (identifier) @alias @imported))
+       (import_from_statement
+         module_name: (dotted_name) @source
+         name: (dotted_name) @name @imported)
+       (import_from_statement
+         module_name: (dotted_name) @source
+         name: (aliased_import
+                 name: (dotted_name) @name
+                 alias: (identifier) @alias @imported))
+      ] @statement)
+    ]]
+  )
+  local imports = {}
+  for _, match, _ in query:iter_matches(tree:root(), bufnr) do
+    local current = {}
+    for id, node in pairs(match) do
+      current[query.captures[id]] = node
+    end
+    imports[#imports + 1] = current
+  end
+  return imports
+end
+
+local function get_node_text(bufnr, node)
+  local start_row, start_col, end_row, end_col = node:range()
+  local text = vim.api.nvim_buf_get_text(bufnr, start_row, start_col, end_row, end_col, {})
+  return table.concat(text, "\n")
+end
+
+local function import_exists(bufnr, import_spec, parsed_imports)
+  for _, nodes in ipairs(parsed_imports) do
+    local imported = import_spec.name
+    if import_spec.alias ~= nil then
+      imported = import_spec.alias
+    end
+    if nodes.imported ~= nil and imported == get_node_text(bufnr, nodes.imported) then
+      return true
+    end
+  end
+  return false
+end
+
+local function render_import(spec)
+  if spec.source ~= nil then
+    if spec.alias ~= nil then
+      return string.format("from %s import %s as %s", spec.source, spec.name, spec.alias)
+    else
+      return string.format("from %s import %s", spec.source, spec.name)
+    end
+  else
+    if spec.alias ~= nil then
+      return string.format("import %s as %s", spec.name, spec.alias)
+    else
+      return string.format("import %s", spec.name)
+    end
+  end
+end
+
+local function import_placement(bufnr, tree, parsed_imports, placement_opts)
+  if #parsed_imports == 0 then
+    return first_import_placement(bufnr, tree)
+  end
+  local _, _, last_line, _ = parsed_imports[#parsed_imports].statement:range()
+  local after_last = last_line + 1
+  local cache = {}
+  if placement_opts == nil then
+    placement_opts = {}
+  end
+  for _, import_nodes in ipairs(parsed_imports) do
+    cache = {}
+    for _, heuristic in ipairs(placement_opts) do
+      local heuristic_matches_this_import = true
+      local before_after, matching = heuristic[1], heuristic[2]
+      for attribute, value in pairs(matching) do
+        if cache[attribute] == nil and import_nodes[attribute] ~= nil then
+          cache[attribute] = get_node_text(bufnr, import_nodes[attribute])
+        end
+        if cache[attribute] ~= value then
+          heuristic_matches_this_import = false
+          break
+        end
+      end
+      local _, _, line, _ = import_nodes.statement:range()
+      if heuristic_matches_this_import then
+        if before_after == "after" then
+          line = line + 1
+        end
+        return line
+      end
+    end
+  end
+  return after_last
+end
+
+function M.maybe_add_import(bufnr, import_spec, opts)
+  if opts == nil then
+    opts = {}
+  end
+  local tree = vim.treesitter.get_parser(bufnr):parse()[1]
+  local parsed_imports = parse_top_level_imports(bufnr, tree)
+  if not import_exists(bufnr, import_spec, parsed_imports) then
+    local lines = { render_import(import_spec) }
+    local placement = import_placement(bufnr, tree, parsed_imports, opts.placement)
+    vim.api.nvim_buf_set_lines(bufnr, placement, placement, false, lines)
+  end
+end
+
+return M
