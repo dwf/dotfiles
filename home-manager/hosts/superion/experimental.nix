f1bbe4e488205d929e294d38793da0f45e70a76e
David Warde-Farley <dwf@google.com>
2024-10-25 23:32:51 +0100

    mh/superion: start playing with starship

--- home-manager/hosts/superion/experimental.nix
+++ home-manager/hosts/superion/experimental.nix
@@ -2,5 +2,11 @@
   programs = {
     zsh.enable = true;
     fish.enable = true;
+    starship = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+    };
   };
 }
