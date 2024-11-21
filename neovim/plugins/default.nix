8bf7927fe674526ccc0063625be4829204b93f39
David Warde-Farley <dwf@google.com>
2024-11-21 23:04:00 +0000

    neovim: trim.nvim -> 2024-11-21
    Fixes highlighting in terminal buffers and looks like some performance
    improvements too.

--- neovim/plugins/default.nix
+++ neovim/plugins/default.nix
@@ -40,6 +40,16 @@
       };
       trim = {
         enable = true;
+        package = pkgs.vimUtils.buildVimPlugin {
+          pname = "trim.nvim";
+          src = pkgs.fetchFromGitHub {
+            owner = "cappyzawa";
+            repo = "trim.nvim";
+            rev = "84a1016c7484943e9fbb961f807c3745342b2462";
+            sha256 = "sha256-RzLttgP3eNQK8iQ86/7SwvB/GF8LCNlBhvZevOXMhSM=";
+          };
+          version = "2024-11-21";
+        };
         settings = {
           ft_blocklist = [
             "diff"
