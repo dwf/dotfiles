56ef7e0441ff3951de855c3eb8284038e4796d72
David Warde-Farley <dwf@google.com>
2024-11-25 14:37:27 +0000

    hm/tmux: ditch custom theme for catppuccin

--- home-manager/profiles/tmux/default.nix
+++ home-manager/profiles/tmux/default.nix
@@ -7,7 +7,36 @@
     extraConfig = builtins.readFile ./tmux.conf;
     historyLimit = 100000;
     keyMode = "vi";
-    plugins = with pkgs.tmuxPlugins; [ tmux-fzf ];
-    # terminal = "screen-256color";
+
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = catppuccin;
+        extraConfig = # tmux
+          ''
+            set -g status-position top
+            set -g @catppuccin_window_left_separator ""
+            set -g @catppuccin_window_right_separator " "
+            set -g @catppuccin_window_middle_separator " █"
+            set -g @catppuccin_window_number_position "right"
+            set -g @catppuccin_window_default_fill "number"
+            set -g @catppuccin_window_default_text "#W"
+            set -g @catppuccin_window_current_fill "number"
+            set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+            set -g @catppuccin_status_modules_right "host directory date_time"
+            set -g @catppuccin_status_modules_left "session"
+            set -g @catppuccin_status_left_separator  " "
+            set -g @catppuccin_status_right_separator " "
+            set -g @catppuccin_status_right_separator_inverse "no"
+            set -g @catppuccin_status_fill "icon"
+            set -g @catppuccin_status_connect_separator "no"
+            set -g @catppuccin_directory_text "#{b:pane_current_path}"
+            set -g @catppuccin_date_time_text "%H:%M"
+
+            # transparent status line, outside of modules
+            set -g @catppuccin_status_background "default"
+          '';
+      }
+      tmux-fzf
+    ];
   };
 }
