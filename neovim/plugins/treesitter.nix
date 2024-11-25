de3f2bbfd73d54155af1c60fcf82c8750bb25b9c
David Warde-Farley <dwf@google.com>
2024-11-25 13:18:11 +0000

    neovim: disable treesitter for tmux configs

--- neovim/plugins/treesitter.nix
+++ neovim/plugins/treesitter.nix
@@ -1,6 +1,9 @@
 {
   config.plugins.treesitter = {
     enable = true;
+    disabledLanguages = [
+      "tmux" # the treesitter grammar has a bug with 'set -g status' [no value]
+    ];
     # settings.incremental_selection in a future nixvim
     incrementalSelection = {
       enable = true;
