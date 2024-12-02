6787c08ac1d56b11b6c445a3ad34638ea1652a62
David Warde-Farley <dwf@google.com>
2024-12-02 23:36:10 +0000

    fzf/fish: add fifc, with patch

--- home-manager/profiles/fish.nix
+++ home-manager/profiles/fish.nix
@@ -9,6 +9,30 @@
           name = "fzf.fish";
           inherit (fzf-fish) src;
         }
+        {
+          name = "fifc";
+          src = pkgs.stdenvNoCC.mkDerivation {
+            pname = "fifc";
+            version = "2024-12-02";
+            src = pkgs.fetchFromGitHub {
+              owner = "gazorby";
+              repo = "fifc";
+              rev = "e953fcd521f34651d4eabedcc08cfdef7945b31d";
+              sha256 = "sha256-VmeVVwX5/rPZr98vaitvw6bqcZgaBNc+mEYkDRjFBXA=";
+            };
+            patches = [
+              (pkgs.fetchpatch {
+                url = "https://github.com/gazorby/fifc/pull/52/commits/d7a44c151e72185208781ad69420bef824093cce.patch";
+                sha256 = "sha256-gpH29b2BgURr+wHSZErLbhaOBpCrwLxkQZBhw4m4/Pw=";
+              })
+            ];
+            dontConfigure = true;
+            dontBuild = true;
+            installPhase = ''
+              cp -R $src $out
+            '';
+          };
+        }
       ];
     };
     fzf.enableFishIntegration = false;
