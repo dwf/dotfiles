e2a49c0c7fd1ff4760fd31180cf606c3df1f4f74
David Warde-Farley <dwf@google.com>
2024-12-02 23:10:48 +0000

    hm/tmux: switch to tokyonight tmux theme

--- home-manager/profiles/tmux/default.nix
+++ home-manager/profiles/tmux/default.nix
@@ -9,6 +9,7 @@
         set -g status-position top
       ''
       (builtins.readFile ./tmux.conf)
+      (builtins.readFile "${pkgs.vimPlugins.tokyonight-nvim}/extras/tmux/tokyonight_moon.tmux")
     ];
 
     historyLimit = 100000;
@@ -16,32 +17,6 @@
     terminal = "screen-256color";
 
     plugins = with pkgs.tmuxPlugins; [
-      {
-        plugin = catppuccin;
-        extraConfig = # tmux
-          ''
-            set -g @catppuccin_window_left_separator ""
-            set -g @catppuccin_window_right_separator " "
-            set -g @catppuccin_window_middle_separator " █"
-            set -g @catppuccin_window_number_position "right"
-            set -g @catppuccin_window_default_fill "number"
-            set -g @catppuccin_window_default_text "#W"
-            set -g @catppuccin_window_current_fill "number"
-            set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-            set -g @catppuccin_status_modules_right "host directory date_time"
-            set -g @catppuccin_status_modules_left "session"
-            set -g @catppuccin_status_left_separator  " "
-            set -g @catppuccin_status_right_separator " "
-            set -g @catppuccin_status_right_separator_inverse "no"
-            set -g @catppuccin_status_fill "icon"
-            set -g @catppuccin_status_connect_separator "no"
-            set -g @catppuccin_directory_text "#{b:pane_current_path}"
-            set -g @catppuccin_date_time_text "%H:%M"
-
-            # transparent status line, outside of modules
-            set -g @catppuccin_status_background "default"
-          '';
-      }
       vim-tmux-navigator
     ];
   };
