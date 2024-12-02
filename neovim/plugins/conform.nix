f7c7bb0fda7354dd200c72cc3f65dfd189ebba96
David Warde-Farley <dwf@google.com>
2024-12-02 15:19:29 +0000

    neovim: work-around nixvim/conform bug, format

--- neovim/plugins/conform.nix
+++ neovim/plugins/conform.nix
@@ -8,20 +8,22 @@
     plugins.conform-nvim = {
       enable = true;
       settings = {
-      formatters.stylua = {
-        prepend_args = [
-          "--indent-type"
-          "spaces"
-          "--indent-width"
-          "2"
-        ];
+        formatters.stylua = {
+          prepend_args = [
+            "--indent-type"
+            "spaces"
+            "--indent-width"
+            "2"
+          ];
+        };
+        formatters_by_ft = {
+          lua = [ "stylua" ];
+          nix = [ "nixfmt" ];
+        };
+        format_on_save = {
+          quiet = false;
+        };
       };
-      formatters_by_ft = {
-        lua = [ "stylua" ];
-        nix = [ "nixfmt" ];
-      };
-      format_on_save = { };
     };
-  };
   };
 }
