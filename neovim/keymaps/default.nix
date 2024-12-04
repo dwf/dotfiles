5d8f7030daaefe4b3d9e7f362a481e39298cf0c2
David Warde-Farley <dwf@google.com>
2024-12-04 22:33:59 +0000

    neovim: %s/// keymaps

--- neovim/keymaps/default.nix
+++ neovim/keymaps/default.nix
@@ -3,4 +3,31 @@
     ./nix.nix
     ./python.nix
   ];
+
+  config.keymaps = [
+    {
+      action = "\"hy:%s/<C-r>h//g<left><left>";
+      key = "<leader>S";
+      options = {
+        desc = "Search and replace selection.";
+      };
+      mode = "v";
+    }
+    {
+      action = "\"syiw:%s/<C-r>s//g<left><left>";
+      key = "<leader>S";
+      options = {
+        desc = "Search and replace word under cursor.";
+      };
+      mode = "n";
+    }
+    {
+      action = ":%s///g<left><left><left>";
+      key = "<leader>s";
+      options = {
+        desc = "Search and replace.";
+      };
+      mode = "n";
+    }
+  ];
 }
