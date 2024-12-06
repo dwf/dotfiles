7401e15fa89c0485841622e8e2e634d914ec2b05
David Warde-Farley <dwf@google.com>
2024-12-06 22:08:19 +0000

    neovim: replace overseer with nixvim module

--- neovim/plugins/overseer.nix
+++ neovim/plugins/overseer.nix
@@ -1,22 +1,3 @@
-{ pkgs, ... }:
 {
-  config = {
-    extraPlugins = [
-      (pkgs.vimUtils.buildVimPlugin {
-        pname = "overseer-nvim";
-        src = pkgs.fetchFromGitHub {
-          owner = "stevearc";
-          repo = "overseer.nvim";
-          rev = "a2734d90c514eea27c4759c9f502adbcdfbce485";
-          sha256 = "sha256-jRT2a8C/8V0tEwBlGRfQeVsUKzR2YinqinBkayvQRLo=";
-        };
-        version = "2024-09-22";
-      })
-    ];
-
-    extraConfigLua = ''
-      require('overseer').setup {}
-    '';
-  };
-
+  config.plugins.overseer.enable = true;
 }
