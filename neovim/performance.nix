b4f32a2fed2aefc88ee8385e1abbd5da657c6eeb
David Warde-Farley <dwf@google.com>
2025-01-04 17:52:03 +0000

    neovim: fix plugin-combining collision

--- neovim/performance.nix
+++ neovim/performance.nix
@@ -1,5 +1,6 @@
+{ pkgs, ... }:
 {
-    performance = {
+  performance = {
     byteCompileLua = {
       enable = true;
       nvimRuntime = true;
@@ -13,9 +14,16 @@
         "nvim-treesitter"
         "nvim-treesitter-textobjects"
         "vimplugin-treesitter-grammar-nix"
-        "overseer-nvim"
       ];
     };
   };
   luaLoader.enable = true;
+
+  # I can't get standalonePlugins to solve the overseer/conform collision so
+  # just patch the derivation.
+  plugins.overseer.package = pkgs.vimPlugins.overseer-nvim.overrideAttrs {
+    postInstall = ''
+      mv $out/doc/recipes.md $out/doc/overseer-recipes.md
+    '';
+  };
 }
