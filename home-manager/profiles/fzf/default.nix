b61c9323b1ec3b8644e290513e6f8a1b6b264f6b
David Warde-Farley <dwf@google.com>
2025-01-25 17:24:20 +0000

    hm/profiles/fzf: add ripgrep integration

--- home-manager/profiles/fzf/default.nix
+++ home-manager/profiles/fzf/default.nix
@@ -1,34 +1,52 @@
-{ pkgs, ... }:
 {
-  programs.fzf = {
-    enable = true;
-    fileWidgetCommand = "fd --type f --color=always";
-    fileWidgetOptions = [
-      "--ansi"
-      "--preview '${pkgs.bat}/bin/bat --color=always {}'"
-    ];
-    changeDirWidgetCommand = "fd --type d --color=always";
-    changeDirWidgetOptions = [
-      "--ansi"
-      "--preview '${pkgs.eza}/bin/eza --color=always --group-directories-first --icons -F {} -1'"
-    ];
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  programs = {
+    fzf = {
+      enable = true;
+      fileWidgetCommand = "fd --type f --color=always";
+      fileWidgetOptions = [
+        "--ansi"
+        "--preview '${pkgs.bat}/bin/bat --color=always {}'"
+      ];
+      changeDirWidgetCommand = "fd --type d --color=always";
+      changeDirWidgetOptions = [
+        "--ansi"
+        "--preview '${pkgs.eza}/bin/eza --color=always --group-directories-first --icons -F {} -1'"
+      ];
+    };
+    bash = lib.mkIf config.programs.fzf.enableBashIntegration {
+      initExtra = lib.mkAfter (builtins.readFile ./ripgrep.sh);
+    };
+    zsh = lib.mkIf config.programs.fzf.enableZshIntegration {
+      initExtra = lib.mkAfter (builtins.readFile ./ripgrep.sh);
+    };
   };
 
-  home.packages = [
-    (pkgs.writeShellScriptBin "git-fzs" (
-      builtins.readFile (
-        pkgs.substituteAll {
-          src = ./fzf_git_status.sh;
-          inherit (pkgs)
-            bat
-            boxes
-            delta
-            eza
-            fzf
-            git
-            ;
-        }
-      )
-    ))
-  ];
+  home = {
+    shellAliases = {
+      frg = "rfv";
+    };
+    packages = [
+      (pkgs.writeShellScriptBin "git-fzs" (
+        builtins.readFile (
+          pkgs.substituteAll {
+            src = ./fzf_git_status.sh;
+            inherit (pkgs)
+              bat
+              boxes
+              delta
+              eza
+              fzf
+              git
+              ;
+          }
+        )
+      ))
+    ];
+  };
 }
