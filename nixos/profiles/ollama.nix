50131402d4b6db8fd1dced0433d39b4afb14bbee
David Warde-Farley <dwf@google.com>
2025-01-26 19:55:31 +0000

    nixos/profiles/ollama.nix: simplify naming, lint + format

--- nixos/profiles/ollama.nix
+++ nixos/profiles/ollama.nix
@@ -1,31 +1,39 @@
 # Ollama in a container with ROCm support. Set OLLAMA_HOST=addr:port when
 # running ollama outside the container to talk to the container instance.
 {
-  nixpkgs   # pass a nixpkgs that includes rocmPackages_6, e.g. unstable
-, containerHostAddr ? "10.233.10.1"
-, containerGuestAddr ? "10.233.10.2"
-, autoStart ? true
-, devices ? [
-  "/dev/dri/card0"
-  "/dev/kfd"
-  "/dev/dri/renderD128"  # seems to only really need this one
-]
-, port ? 11434
-, ...
+  nixpkgs,
+  hostAddress ? "10.233.10.1",
+  localAddress ? "10.233.10.2",
+  autoStart ? true,
+  devices ? [
+    "/dev/dri/card0"
+    "/dev/kfd"
+    "/dev/dri/renderD128" # seems to only really need this one
+  ],
+  port ? 11434,
+  ...
 }:
 {
   containers.ollama = {
-    inherit nixpkgs autoStart;
+    inherit
+      nixpkgs
+      autoStart
+      hostAddress
+      localAddress
+      ;
     privateNetwork = true;
-    hostAddress = containerHostAddr;
-    localAddress = containerGuestAddr;
-    allowedDevices = let
-      deviceDescr = node: { inherit node; modifier = "rw"; };
-    in map deviceDescr devices;
-    bindMounts = (nixpkgs.lib.genAttrs devices (name: {})) // {
+    allowedDevices =
+      let
+        deviceDescr = node: {
+          inherit node;
+          modifier = "rw";
+        };
+      in
+      map deviceDescr devices;
+    bindMounts = (nixpkgs.lib.genAttrs devices (_: { })) // {
       "/sys/module".isReadOnly = true;
     };
-    config = { pkgs, ... }: {
+    config = {
       networking.firewall.allowedTCPPorts = [ port ];
 
       # Strangely, package = pkgs.ollama.override { ... } didn't work
