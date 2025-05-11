cb73b4bbe2e0140af690c495c54498c8d96a4b72
David Warde-Farley <dwf@google.com>
2025-05-11 19:25:11 +0100

    nixos: zsh profile

--- nixos/hosts/superion/default.nix
+++ nixos/hosts/superion/default.nix
@@ -8,6 +8,7 @@
     ../../profiles/framework-amd.nix
     ../../profiles/laptop.nix
     ../../profiles/wayland.nix
+    ../../profiles/zsh.nix
   ];
 
   programs.dconf.enable = true;
@@ -24,20 +25,11 @@
   fileSystems."/nix".options = [ "noatime" ];
 
   # This didn't get added to hardware-configuration.nix, for some reason.
-  boot.initrd.luks.devices."cryptswap".device = "/dev/disk/by-uuid/a550eea0-45e0-47b0-89a3-b5cf85625f62";
+  boot.initrd.luks.devices."cryptswap".device =
+    "/dev/disk/by-uuid/a550eea0-45e0-47b0-89a3-b5cf85625f62";
 
   # linuxPackages_latest on 24.11 broke the fingerprint reader.
   # boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_11;
-
-  # Allows home-manager installed shells to be the login shell.
-  environment.etc."shells".text = lib.mkAfter ''
-    /home/dwf/.nix-profile/bin/zsh
-  '';
-
-  users.users.dwf = {
-    useDefaultShell = false;
-    shell = "/home/dwf/.nix-profile/bin/zsh";
-  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
---
+++ nixos/profiles/zsh.nix
@@ -0,0 +1,13 @@
+{ lib, ... }:
+{
+  # Allows home-manager installed shells to be the login shell.
+  environment.etc."shells".text = lib.mkAfter ''
+    /home/dwf/.nix-profile/bin/zsh
+  '';
+
+  # Set my default login shell to home-manager-installed zsh.
+  users.users.dwf = {
+    useDefaultShell = false;
+    shell = "/home/dwf/.nix-profile/bin/zsh";
+  };
+}
