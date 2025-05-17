a5e921c6e42c14f314f31116caeb2f5e67c936d6
David Warde-Farley <dwf@google.com>
2025-05-17 23:57:28 +0100

    neovim: fix trim.nvim whitespace highlighting in terminals
    Fixes #26

--- neovim/plugins/trim.nix
+++ neovim/plugins/trim.nix
@@ -1,6 +1,33 @@
+{ pkgs, lib, ... }:
 {
   config.plugins.trim = {
     enable = true;
+    package = pkgs.vimPlugins.trim-nvim.overrideAttrs (_: {
+      version = "2025-05-17";
+      src = pkgs.fetchFromGitHub {
+        owner = "cappyzawa";
+        repo = "trim.nvim";
+        rev = "d0760a840ca2fe4958353dee567a90c2994e70a7";
+        sha256 = "sha256-CZwIa9GccHS/nZ+lq27A6NfpBCqEHOrTC7Hd7skPwnc=";
+      };
+      patches = [
+        (pkgs.writeText "terminal-whitespace-fix.patch" ''
+          diff --git a/lua/trim/highlighter.lua b/lua/trim/highlighter.lua
+          index 39bb9dc..c102b6c 100644
+          --- a/lua/trim/highlighter.lua
+          +++ b/lua/trim/highlighter.lua
+          @@ -56,7 +56,7 @@ function highlighter.setup()
+             })
+
+             local augroup = vim.api.nvim_create_augroup('TrimHighlight', { clear = true })
+          -  vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter', 'TermEnter' }, {
+          +  vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter', 'TermEnter', 'TermOpen' }, {
+               group = augroup,
+               callback = function()
+                 if vim.bo.buftype == ''' and not has_value(config.ft_blocklist, vim.bo.filetype) then
+        '')
+      ];
+    });
     lazyLoad.settings.event = "DeferredUIEnter";
     settings = {
       ft_blocklist = [
