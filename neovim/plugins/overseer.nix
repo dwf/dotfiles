894e63c5948f3d8e9e75eae1f4f0dc745d5cb77e
David Warde-Farley <dwf@google.com>
2025-06-07 15:25:40 +0100

    neovim: add OverseerRestartLast command

--- neovim/plugins/overseer.nix
+++ neovim/plugins/overseer.nix
@@ -1,6 +1,25 @@
+{ helpers, ... }:
 {
-  config.plugins.overseer = {
-    enable = true;
-    lazyLoad.settings.event = "DeferredUIEnter";
+  config = {
+    plugins.overseer = {
+      enable = true;
+      lazyLoad.settings.event = "DeferredUIEnter";
+    };
+    userCommands = {
+      # https://github.com/stevearc/overseer.nvim/blob/dc67e8500b81dcfe18192e900f952be73966c35f/doc/recipes.md
+      OverseerRestartLast = {
+        command = helpers.mkRaw ''
+          function()
+            local overseer = require("overseer")
+            local tasks = overseer.list_tasks({ recent_first = true })
+            if vim.tbl_isempty(tasks) then
+              vim.notify("No tasks found", vim.log.levels.WARN)
+            else
+              overseer.run_action(tasks[1], "restart")
+            end
+          end
+        '';
+      };
+    };
   };
 }
