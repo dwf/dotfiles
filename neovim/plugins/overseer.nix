e6d51e964a501d51f4006e2300a8282343b3e5b5
David Warde-Farley <dwf@google.com>
2025-06-09 15:49:23 +0100

    neovim: overseer keybindings

--- neovim/plugins/overseer.nix
+++ neovim/plugins/overseer.nix
@@ -3,8 +3,51 @@
   config = {
     plugins.overseer = {
       enable = true;
+      # TODO: lazy load on commands/keys
       lazyLoad.settings.event = "DeferredUIEnter";
     };
+    keymaps = [
+      {
+        key = "<leader>or";
+        action = "<cmd>OverseerRun<cr>";
+        mode = [ "n" ];
+        options = {
+          desc = "overseer.nvim: run task";
+        };
+      }
+      {
+        key = "<leader>ot";
+        action = "<cmd>OverseerToggle<cr>";
+        mode = [ "n" ];
+        options = {
+          desc = "overseer.nvim: toggle tasks pane";
+        };
+      }
+      {
+        key = "<leader>ol";
+        action = "<cmd>OverseerRestartLast<cr>";
+        mode = [ "n" ];
+        options = {
+          desc = "overseer.nvim: restart last task";
+        };
+      }
+      {
+        key = "<leader>oa";
+        action = "<cmd>OverseerTaskAction<cr>";
+        mode = [ "n" ];
+        options = {
+          desc = "overseer.nvim: task action";
+        };
+      }
+      {
+        key = "<leader>oi";
+        action = "<cmd>OverseerInfo<cr>";
+        mode = [ "n" ];
+        options = {
+          desc = "overseer.nvim: diagnostic info";
+        };
+      }
+    ];
     userCommands = {
       # https://github.com/stevearc/overseer.nvim/blob/dc67e8500b81dcfe18192e900f952be73966c35f/doc/recipes.md
       OverseerRestartLast = {
