c2f4a4f429eb5507eb965279d692d5ba3925ca0e
David Warde-Farley <dwf@google.com>
2025-06-10 22:52:54 +0100

    neovim: DeferredUIEnter to constant

--- neovim/plugins/default.nix
+++ neovim/plugins/default.nix
@@ -19,50 +19,54 @@
     ./trouble.nix
   ];
 
-  config = {
-    extraPlugins = [
-      (pkgs.vimUtils.buildVimPlugin {
-        pname = "treesitter-helpers";
-        src = ../treesitter;
-        version = "2024-09-05";
-        nvimSkipModules = [ "treesitter-helpers.python" ];
-      })
-    ];
-
-    plugins =
-      {
-        indent-blankline = {
-          enable = true;
-          settings.scope = {
-            enabled = true;
-            show_start = false;
-            show_end = false;
-          };
-          lazyLoad.settings.event = "DeferredUIEnter";
-        };
-        lz-n.enable = true;
-        gitblame = {
-          enable = true;
-          settings.delay = 5000;
-          lazyLoad.settings.event = "DeferredUIEnter";
-        };
-        git-conflict.enable = true;
-        lspkind.enable = true;
-        nix.enable = true;
-        tmux-navigator.enable = true;
-      }
-      // (lib.genAttrs
-        [
-          "gitsigns"
-          "project-nvim"
-          "twilight"
-          "web-devicons"
-          "which-key"
-        ]
-        (_: {
-          enable = true;
-          lazyLoad.settings.event = "DeferredUIEnter";
+  config =
+    let
+      DeferredUIEnter = "DeferredUIEnter";
+    in
+    {
+      extraPlugins = [
+        (pkgs.vimUtils.buildVimPlugin {
+          pname = "treesitter-helpers";
+          src = ../treesitter;
+          version = "2024-09-05";
+          nvimSkipModules = [ "treesitter-helpers.python" ];
         })
-      );
-  };
+      ];
+
+      plugins =
+        {
+          indent-blankline = {
+            enable = true;
+            settings.scope = {
+              enabled = true;
+              show_start = false;
+              show_end = false;
+            };
+            lazyLoad.settings.event = DeferredUIEnter;
+          };
+          lz-n.enable = true;
+          gitblame = {
+            enable = true;
+            settings.delay = 5000;
+            lazyLoad.settings.event = DeferredUIEnter;
+          };
+          git-conflict.enable = true;
+          lspkind.enable = true;
+          nix.enable = true;
+          tmux-navigator.enable = true;
+        }
+        // (lib.genAttrs
+          [
+            "gitsigns"
+            "project-nvim"
+            "twilight"
+            "web-devicons"
+            "which-key"
+          ]
+          (_: {
+            enable = true;
+            lazyLoad.settings.event = DeferredUIEnter;
+          })
+        );
+    };
 }
