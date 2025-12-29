8769847fa7a24b2f0320fecefda4a6b737db646e
David Warde-Farley <dwf@google.com>
2025-12-28 23:29:59 -0500

    nixos/zsh: add pathsToLink per HM docs

--- nixos/profiles/zsh.nix
+++ nixos/profiles/zsh.nix
@@ -1,9 +1,14 @@
 { lib, ... }:
 {
   # Allows home-manager installed shells to be the login shell.
-  environment.etc."shells".text = lib.mkAfter ''
-    /home/dwf/.nix-profile/bin/zsh
-  '';
+  environment = {
+    etc."shells".text = lib.mkAfter ''
+      /home/dwf/.nix-profile/bin/zsh
+    '';
+
+    # For completion of system packages.
+    pathsToLink = [ "/share/zsh" ];
+  };
 
   # Set my default login shell to home-manager-installed zsh.
   users.users.dwf = {
