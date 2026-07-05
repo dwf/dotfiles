9f3d5ae846c184ff998d6917a324366a6f0432d8
David Warde-Farley <d.warde.farley@gmail.com>
2026-07-05 23:16:51 +0100

    nixos/hosts/superion: improve sdcard wrapper

--- nixos/hosts/superion/ext4-sdcard.nix
+++ nixos/hosts/superion/ext4-sdcard.nix
@@ -1,67 +1,61 @@
 { pkgs, ... }:
 {
-  # Set up two mounts: /mnt/_sdcard, which is a simple ext4 mount, and
-  # /mnt/sdcard, which is a bindfs mount which remaps uid 0 (root) to
-  # uid 1000 (dwf).
-  fileSystems = {
-    "/mnt/_sdcard" = {
-      device = "/dev/sda1";
-      fsType = "ext4";
-      options = [
-        "noauto"
-        "user"
-      ];
-    };
-    "/mnt/sdcard" = {
-      device = "/mnt/_sdcard";
-      fsType = "fuse.bindfs";
-      options = [
-        "map=0/1000"
-        "noauto"
-      ];
-      depends = [ "/mnt/_sdcard" ];
-    };
-  };
+  # This module provides an `sdcard` shell script with subcommands "mount" and
+  # "unmount" which mounts /dev/sda1 on /mnt/sdcard.
 
-  # Shell script for easy management of the raw and bindfs mounts.
+  # It expects either exfat or ext4 formatted cards, and for ext4 it is designed
+  # with a particular use case in mind: namely, it is being used in an embedded
+  # device that creates lots of stuff as root, and expects files to be owned by
+  # root. The script thus uses bindfs to remap root to my user. Finally, the
+  # script is added as a sudoers rule so it can be run without a password.
+  # Note that fstab entries are intentionally not created given the lack of a
+  # fixed filesystem type.
+
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "sdcard" ''
-      if [ "$1" != "mount" -a "$1" != "unmount" -a "$1" != "umount" -o $# -ne 1 ]; then
-        echo "usage: $0 <mount/u[n]mount>"
-        exit 1
-      fi
-      if [ "$1" == "mount" ]; then
-        if [ -n "$(mount |grep '/mnt/sdcard')" ]; then
-          echo "Already mounted"
+      UID=$(id -u)
+      case $1 in
+        mount)
+          FSTYPE=$(sudo ${pkgs.util-linux}/bin/blkid -o value -s TYPE /dev/sda1)
+          case $FSTYPE in
+            exfat|vfat)
+              sudo mount -t $FSTYPE -o uid=$UID,gid=100,umask=0022 /dev/sda1 /mnt/sdcard
+              ;;
+            ext4)
+              sudo mount /dev/sda1 /mnt/_sdcard || exit 1
+              ${pkgs.bindfs}/bin/bindfs --map=0/$UID /mnt/_sdcard /mnt/sdcard || \
+                { sudo umount /mnt/_sdcard; exit 1; }
+              ;;
+            *)
+              echo "Unknown filesystem type: $FSTYPE"
+              exit 1
+              ;;
+          esac
+          ;;
+        unmount|umount)
+          sudo umount /mnt/sdcard || { echo "Failed to unmount /mnt/sdcard"; exit 1; }
+          if mountpoint -q /mnt/_sdcard; then
+            sudo umount /mnt/_sdcard || echo "Warning: failed to unmount /mnt/_sdcard"
+          fi
+          ;;
+        *)
+          echo "Usage: sdcard mount|unmount"
           exit 1
-        fi
-        mount /mnt/_sdcard
-        sudo mount /mnt/sdcard
-      else
-        sudo umount /mnt/sdcard
-        umount /mnt/_sdcard
-      fi
+          ;;
+      esac
     '')
   ];
 
   systemd.tmpfiles.rules = [
-    "d /mnt/_sdcard 0755 dwf users -"
-    "d /mnt/sdcard 0755 dwf users -"
+    "d /mnt/_sdcard 0755 root root -"
+    "d /mnt/sdcard 0755 root root -"
   ];
 
-  security.sudo.extraRules = [
-    {
-      users = [ "dwf" ];
-      commands = [
-        {
-          command = "/run/wrappers/bin/mount /mnt/sdcard";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "/run/wrappers/bin/umount /mnt/sdcard";
-          options = [ "NOPASSWD" ];
-        }
-      ];
-    }
-  ];
+  security.wrappers.sdcard = {
+    source = "/run/current-system/sw/bin/sdcard";
+    owner = "root";
+    group = "root";
+    setuid = true;
+  };
+
 }
