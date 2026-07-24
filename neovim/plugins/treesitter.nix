978ff5cd731426c1d90b51f1ecc6af8e8a8e6e37
David Warde-Farley <d.warde.farley@gmail.com>
2026-07-24 23:25:21 +0100

    neovim: remove deprecated nvim-treesitter stuff

--- neovim/plugins/treesitter.nix
+++ neovim/plugins/treesitter.nix
@@ -15,18 +15,5 @@
         "tmux" # the treesitter grammar has a bug with 'set -g status' [no value]
       ];
     };
-    # settings.incremental_selection in a future nixvim
-    settings = {
-      incrementalSelection = {
-        enable = true;
-        keymaps = {
-          # snake-case in a future nixvim
-          nodeIncremental = "=";
-          nodeDecremental = "-";
-          scopeIncremental = "+";
-        };
-      };
-      nixvimInjections = true;
-    };
   };
 }
