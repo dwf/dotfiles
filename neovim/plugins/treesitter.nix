243d290ed44f6d4a7b509feee226b968398db9c6
David Warde-Farley <d.warde.farley@gmail.com>
2026-07-24 23:21:42 +0100

    neovim: run vim.treesitter.start()

--- neovim/plugins/treesitter.nix
+++ neovim/plugins/treesitter.nix
@@ -1,7 +1,14 @@
 {
   config.plugins.treesitter = {
     enable = true;
-    lazyLoad.settings.event = "DeferredUIEnter";
+    lazyLoad.settings = {
+      event = "DeferredUIEnter";
+      after = ''
+        function()
+          vim.treesitter.start()
+        end
+      '';
+    };
     highlight = {
       enable = true;
       disable = [
